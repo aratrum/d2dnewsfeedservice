1b5fd24b2a733919ceaaf50529ea8fef42e7b9b8
robbertploeg@gmail.com <robbertploeg@gmail.com@fe5a65bc-7ff5-0a71-8944-95622133bc50>
2013-06-03 08:21:49 +0000

    Added connection to the Graph.facebook.com API TO DO: send the right info over the get

--- trunk/ d2dnewsfeedservice --username robbertploeg@gmail.com/src/main/java/nl/ead/webservice/service/ServiceCaller.java
+++ trunk/ d2dnewsfeedservice --username robbertploeg@gmail.com/src/main/java/nl/ead/webservice/service/ServiceCaller.java
@@ -1,36 +1,71 @@
 package nl.ead.webservice.service;
 
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.methods.GetMethod;
+
+import java.io.IOException;
+
+
 /**
  * User: Robbert
  * Date: 29-5-13
  * Time: 11:25
  */
 public class ServiceCaller {
-    String FACEBOOK_URL = "";
+    String FACEBOOK_URL = "graph.facebook.com";
     String ARTICLEAPI_URL = "";
 
-    public ServiceCaller(){
+    public ServiceCaller() {
 
     }
 
     /**
      * This method uses the FACEBOOK_URL to connect to the facebookAPI.
      * It needs the facebook username to get the correct data.
+     *
      * @param username
      * @return
      */
-    public String connectToFacebook(String username){
-        String response = FACEBOOK_URL;
-        return response;
+    public String connectToFacebook(String username) {
+        String generic_error = "";
+        HttpClient client = new HttpClient();
+        HttpMethod method = new GetMethod("http://graph.facebook.com/" + username);
+
+        try {
+            client.executeMethod(method);
+
+            if (method.getStatusCode() == HttpStatus.SC_OK) {
+                String response = method.getResponseBodyAsString();
+                System.out.println("Response = " + response);
+                System.out.println("GET Success: OK");
+                return response;
+            } else if (method.getStatusCode() == HttpStatus.SC_BAD_REQUEST) {
+                String fail_response = method.getResponseBodyAsString();
+                System.out.println("GET Error: BAD REQUEST, Server Response: \n" + fail_response);
+                return "";
+            }
+            generic_error = method.getResponseBodyAsString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            method.releaseConnection();
+        }
+
+
+        return "GET Request Failed, Server Response: \n" + generic_error;
     }
+
 
     /**
      * This method uses the ARTICLEAPI_UR to connect to the ArticleAPI.
      * It needs the interest name to get the correct data.
+     *
      * @param interest
      * @return
      */
-    public String connectToArticleAPI(String interest){
+    public String connectToArticleAPI(String interest) {
         String response = ARTICLEAPI_URL;
         return response;
     }
