7792303b689c71a62112b34c034a7d46042c5800
heijmansroy@gmail.com <heijmansroy@gmail.com@fe5a65bc-7ff5-0a71-8944-95622133bc50>
2013-06-10 07:16:22 +0000

    The article getter now does valid search es and grabs the first 3 articles it fiends. It puts these in one JSONArray and returns them.

---  d2dnewsfeedservice --username robbertploeg@gmail.com/src/main/java/nl/ead/webservice/core/ArticleParser.java
+++  d2dnewsfeedservice --username robbertploeg@gmail.com/src/main/java/nl/ead/webservice/core/ArticleParser.java
@@ -1,6 +1,10 @@
 package nl.ead.webservice.core;
 
 import nl.ead.webservice.entity.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 /**
@@ -14,9 +18,46 @@ public class ArticleParser {
 
     }
 
-    public ArrayList<Article> processArticles(String input){
-        ArrayList<Article> articleList = null;
+    public ArrayList<Article> processArticles(String input, int size) throws JSONException {
 
+        String newString;
+        ArrayList<Article> articleList = new ArrayList<Article>();
+        JSONArray articleArray = new JSONArray(input);
+        for(int i = 0; i < size; i++ )
+        {
+            System.out.println(articleArray.getString(i));
+            newString = articleArray.getString(i);
+            newString = escapeString(newString);
+
+            JSONObject object = new JSONObject(newString);
+            //JSONObject object = articleArray.getJSONObject(i);
+            Article ar = new Article(object.getString("title"), object.getString("text"), object.getString("url"));
+            articleList.add(ar);
+            object.toString();
+            System.out.println(articleList.get(i).title);
+            System.out.println(articleList.get(i).link);
+            System.out.println(articleList.get(i).description);
+        }
         return articleList;
     }
+
+    public static String escapeString(String text)
+    {
+        String result ="";
+        char symbol;
+        System.out.println("Here now");
+        System.out.println(text);
+        for(int i = 0; i < text.length(); i++)
+        {
+            symbol = text.charAt(i);
+            if(symbol == '"')
+            {
+                result = result + "\\";
+            }
+            result = result + symbol;
+        }
+
+        return result;
+    }
 }
+
